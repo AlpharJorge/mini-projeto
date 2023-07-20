2d4ed98d0d6567a3bf33abbd0bf81147f5a5a18d
Jorge Victor <alpharcontato@gmail.com>
2023-07-20 17:14:26 -0300

    teste

--- aula/teste.js
+++ aula/teste.js
@@ -1,31 +1,6 @@
-// Definindo as três falas em um array
-const falas = [
-    "Não sou eu!!",
-    "Eu tentei te ajudar desde o início!!",
-    "Já pensou que o monstro pode ser controlado?"
-  ];
-  
-  // Inicializa a variável para armazenar o número máximo de letras
-  let maxLetras = 0;
-  // Inicializa a variável para armazenar o índice do suspeito com mais letras
-  let suspeitoComMaisLetras = -1;
-  
-  // Itera pelas falas para encontrar o suspeito com mais letras
-  for (let i = 0; i < falas.length; i++) {
-    const falaAtual = falas[i];
-    const numLetras = falaAtual.replace(/\s/g, "").length;
-  
-    // Verifica se a fala atual tem mais letras que o suspeito anterior
-    if (numLetras > maxLetras) {
-      maxLetras = numLetras;
-      suspeitoComMaisLetras = i;
-    }
-  }
-  
-  // Verifica se encontrou algum suspeito com mais letras
-  if (suspeitoComMaisLetras !== -1) {
-    console.log(`O suspeito com mais letras é o número ${suspeitoComMaisLetras + 1}`);
-  } else {
-    console.log("Não foi encontrado nenhum suspeito.");
-  }
-  
+const pers1 = "Não sou eu!!"
+const pers2 = "Eu  tentei te ajudar desde o início!!"
+const pers3 = "Já pensou que o monstro pode ser controlado?"
+const n1 = pers1.length
+const n2 = pers2.length
+const n3 = pers3.length
